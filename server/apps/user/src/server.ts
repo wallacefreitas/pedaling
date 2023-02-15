cd65afd4a491e4839c5edf95f60efdc411bcef6d
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-02-15 20:06:24 -0300

    feat: Change in structure folders

--- server/apps/user/src/server.ts
+++ server/apps/user/src/server.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { User } from "./entities/user"
+import { User } from "./core/entities/user"
 
 const express = require('express')
 const app = express()
