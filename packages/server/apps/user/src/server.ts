2f210fcd1613d4e8368620f521969868d9cc44ec
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-02-25 10:27:43 -0300

    chore: Change strucutre folder to use monorepo config

--- packages/server/apps/user/src/server.ts
+++ packages/server/apps/user/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from "./routes"
 
 const app = express()
 const port = 3001
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 app.listen(port, () => {
