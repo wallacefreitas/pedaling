0b30ca2378095d24856cb05c1a7a9e46ad322796
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-01 18:21:27 -0300

    feat: Implemented removePedal in controller

---
+++ apps/server/pedal/src/infra/http/controller/remove-pedal-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { RemovePedal} from "../../../application/use-cases/remove-pedal/remove-pedal";
+
+export class RemovePedalController {
+  constructor(
+    private removePedalUseCase: RemovePedal
+  ){}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+
+    try {
+      await this.removePedalUseCase.execute(id)
+      
+      return response.status(200).send();
+    } catch (err: any) {
+      return response.status(400).json({
+        message: err.message || 'Unexpected error.'
+      })
+    }
+  }
+}
