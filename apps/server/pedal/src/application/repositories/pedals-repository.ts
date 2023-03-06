9318a8b967b8da998d42f6be1e704f00b9f40add
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-06 19:23:02 -0300

    chore: Remove commented line

--- apps/server/pedal/src/application/repositories/pedals-repository.ts
+++ apps/server/pedal/src/application/repositories/pedals-repository.ts
@@ -1,7 +1,5 @@
 import { Pedal } from "../entities/pedal";
 
-// type Pedal = PedalProps
-
 export interface PedalsRepository {
   create(pedal: Pedal): Promise<void>;
   save(pedal: Pedal): Promise<void>;
