237f400ee3ad33ce57ce91c5aecb7fc5c8f2a280
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-08 19:28:10 -0300

    chore: Implemented pedal seed to database + install prisma/client

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -3,12 +3,32 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  await users()
+  await pedals()
+}
+
+async function users () {
   await prisma.user.deleteMany()
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       name: "John Doe",
       email: "johndoe@email.com",
       created_at: new Date('2022-12-31T03:00:00.000')
+    }
+  })
+}
+
+async function pedals() {
+  await prisma.pedal.deleteMany()
+  await prisma.pedal.create({
+    data: {
+      name: "Pedaling Test",
+      startDate: new Date('2023-05-30T03:00:00.000'),
+      startDateRegistration: new Date('2023-05-20T03:00:00.000'),
+      endDateRegistration: new Date('2023-05-25T03:00:00.000'),
+      additionalInformation: '',
+      startPlace: 'Roma',
+      participantsLimit: 10
     }
   })
 }
