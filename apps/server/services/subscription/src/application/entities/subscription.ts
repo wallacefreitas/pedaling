357988b4fc28817ae52ff675d5fe55667cc0b350
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-10 15:42:53 -0300

    feat: Created Subscription entity

---
+++ apps/server/services/subscription/src/application/entities/subscription.ts
@@ -0,0 +1,56 @@
+import { z, ZodError } from 'zod';
+
+interface SubscriptionProps {
+  rideId: string
+  userId: string
+  subscriptionDate: Date
+}
+
+export class Subscription {
+  private props: SubscriptionProps
+
+  constructor(props: SubscriptionProps) {
+    const subscriptionSchema = z.object({
+      rideId: z.string(),
+      userId: z.string(),
+      subscriptionDate: z.date()
+    })
+
+    try {
+      const subscription = subscriptionSchema.parse(props)
+      this.props = subscription;
+
+    } catch(err) {
+
+      if (err instanceof ZodError) {
+        err.errors.map(error => { throw new Error(error.message) } )
+      }
+
+      throw new Error("Unknown error")
+    }
+  }
+
+  get rideId() {
+    return this.props.rideId
+  }
+
+  set rideId(rideId: string) {
+    this.props.rideId = rideId
+  }
+
+  get userId() {
+    return this.props.userId
+  }
+
+  set name(userId: string) {
+    this.props.userId = userId
+  }
+
+  get subscriptionDate() {
+    return this.props.subscriptionDate
+  }
+
+  set subscriptionDate(subscriptionDate: Date) {
+    this.props.subscriptionDate = subscriptionDate
+  }
+}
