0cc4b5f3976c4571110bb224f51fb8c9f2b16079
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-29 18:47:06 -0300

    feat: Implemented auth service token with JWT

---
+++ apps/server/middleware/auth.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from 'express';
+import { sign, verify } from 'jsonwebtoken'
+
+const user = {
+  id: '12345',
+  username: 'admin',
+  password: 'admin',
+}
+
+const jwt = {
+  secret: '6845c17d298d95aa942127bdad2ceb9b',
+  expiresIn: '1d'
+}
+
+export class AuthService {
+  private token: string;
+
+  constructor(username: string, password: string) {
+    if (username === user.username && password === user.password) {
+      this.token = sign({}, jwt.secret, {
+        subject: '1',
+        expiresIn: jwt.expiresIn
+      })
+    }
+  }
+
+  public getToken() {
+    return this.token
+  }
+
+  public authorization(request: Request, response: Response, next: NextFunction) {
+    const auth = request.headers.authorization;
+
+    if (!auth) {
+      throw new Error('JWT is missing');
+    }
+
+    try {
+      const [, token] = auth.split(' ');
+      const tokenDecoded = verify(token, jwt.secret);
+
+      if (tokenDecoded) {
+        return next();
+      } 
+        
+    } catch(err) {
+      return response.status(401).json({ message: 'JWT Invalid...'})
+    }
+  }
+}
