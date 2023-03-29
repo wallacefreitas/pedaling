972aeb831b9ab51c61ae8b4161fec447c892bf37
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-03-29 18:48:48 -0300

    feat: Added token route and implemented middelware authorizantion in routes user microservice

--- apps/server/services/user/src/routes.ts
+++ apps/server/services/user/src/routes.ts
@@ -3,22 +3,29 @@ import { createUserController } from "./application/use-cases/create-user";
 import { getAllUsersController } from "./application/use-cases/get-all-users";
 import { getUserByEmailController } from "./application/use-cases/get-user-by-email";
 import { saveUserController } from "./application/use-cases/save-user";
+import { AuthService } from "../../../middleware/auth";
 
 const router = Router()
+const authService = new AuthService("admin", "admin")
 
-router.get('/users', (request, response) => {
+router.post('/', (request, response) => {
+  const token = authService.getToken();
+  return response.json({ token });
+})
+
+router.get('/users', authService.authorization, (request, response) => {
   return getAllUsersController.handle(request, response)
 })
 
-router.post('/users', (request, response) => {
+router.post('/users', authService.authorization, (request, response) => {
   return createUserController.handle(request, response)
 })
 
-router.post('/users/email', (request, response) => {
+router.post('/users/email', authService.authorization, (request, response) => {
   return getUserByEmailController.handle(request, response)
 })
 
-router.put('/users/:id', (request, response) => {
+router.put('/users/:id', authService.authorization, (request, response) => {
   return saveUserController.handle(request, response)
 })
 
