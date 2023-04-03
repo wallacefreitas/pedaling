e0f9455f081f41b7cca464d96b76505c331a2eac
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-03 19:57:39 -0300

    fix: Adjuste in object instance to use Prisma repository

--- apps/server/services/user/src/application/use-cases/create-user/index.ts
+++ apps/server/services/user/src/application/use-cases/create-user/index.ts
@@ -1,11 +1,11 @@
 import { CreateUserController } from "../../../infra/http/controller/create-user-controller"
 import { InMemoryUsersRepository } from "../../../infra/repositories/in-memory/in-memory-users-repository"
-// import { PrismaUsersRepository } from "../../../infra/repositories/prisma/prisma-users-repository"
+import { PrismaUsersRepository } from "../../../infra/repositories/prisma/prisma-users-repository"
 import { CreateUser } from "./create-user"
 
 const inMemoryUsersRepository = new InMemoryUsersRepository()
-//  const prismaUsersRepository = new PrismaUsersRepository() TODO: UNCOMMENTED MAKE A CHANGE TO PRISMA
-const createUser = new CreateUser(inMemoryUsersRepository)
+const prismaUsersRepository = new PrismaUsersRepository();
+const createUser = new CreateUser(prismaUsersRepository);
 const createUserController = new CreateUserController(createUser)
 
 export { createUser, createUserController }
