c113fcc2b5ff05db4054991adacd177121c39922
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-12 15:23:29 -0300

    feat: Create inMemory repository to Subscriptions

--- apps/server/src/infra/repositories/in-memory/in-memory-subscriptions-repository.ts
+++ apps/server/src/infra/repositories/in-memory/in-memory-subscriptions-repository.ts
@@ -1,10 +1,31 @@
-import { Subscription } from "../../../application/entities/subscription";
-import { SubscriptionsRepository } from "../../../application/repositories/subscriptions-repository";
+import { SubscriptionProps as Subscription } from "../../../application/entities/subscription";
+import { SubscriptionPerPedal, SubscriptionsRepository } from "../../../application/repositories/subscriptions-repository";
+import { InMemoryPedalsRepository } from "./in-memory-pedals-repository"
 
 export class InMemorySubscriptionsRepository implements SubscriptionsRepository {
   public subscriptions: Subscription[] = []
 
   async create(subscription: Subscription): Promise<void> {
     this.subscriptions.push(subscription) 
+  }
+
+  async getSubscriptionsPerPedal(pedalId: string): Promise<SubscriptionPerPedal> {
+    const subscriptionsPerPedal = this.subscriptions.filter(subscription => subscription.rideId === pedalId);
+
+    if (subscriptionsPerPedal.length === 0) {
+      return {
+        pedalId: "",
+        users: []
+      }
+    }
+
+    const inMemoryPedalsRepository = new InMemoryPedalsRepository()
+    const pedal = await inMemoryPedalsRepository.findUnique(pedalId)
+    const users: any[] = []
+    
+    return {
+      pedal,
+      users
+    }
   }
 }
