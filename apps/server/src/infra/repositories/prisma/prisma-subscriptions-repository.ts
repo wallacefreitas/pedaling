ae1400bf353559bda7102a1c81387edfaef34fcc
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-12 15:22:07 -0300

    feat: Create subscription repository in Prisma

--- apps/server/src/infra/repositories/prisma/prisma-subscriptions-repository.ts
+++ apps/server/src/infra/repositories/prisma/prisma-subscriptions-repository.ts
@@ -1,25 +1,53 @@
 import { PrismaClient } from "@prisma/client";
 import { Subscription } from "../../../application/entities/subscription";
-import { SubscriptionsRepository } from "../../../application/repositories/subscriptions-repository";
+import { SubscriptionPerPedal, SubscriptionsRepository } from "../../../application/repositories/subscriptions-repository";
+import { PrismaPedalsRepository } from "./prisma-pedals-repository"
 
 export class PrismaSubscriptonsRepository implements SubscriptionsRepository {
   constructor(
-    private prisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: "postgres://localhost/pedaling_user_db"
-        }
-      }
-    })
+    private prisma = new PrismaClient(),
+    private prismaPedalsRepository = new PrismaPedalsRepository()
   ){}
 
   async create(subscription: Subscription): Promise<void> {
-    // await this.prisma.subscription.create({
-    //   data: {
-    //     userId: subscription.userId,
-    //     rideId: subscription.rideId,
-    //     subscriptionDate: subscription.subscriptionDate
-    //   }
-    // });
+    const pedal = await this.prismaPedalsRepository.findUnique(subscription.rideId);
+    const participantsLimit = pedal?.participantsLimit || 0
+    const subscriptionsPerPedal = await this.getSubscriptionsPerPedal(subscription.rideId)
+    const participantsPerPedal = subscriptionsPerPedal.users.length || 0
+
+    if (participantsPerPedal < participantsLimit) {
+      await this.prisma.subscription.create({
+        data: {
+          userId: subscription.userId,
+          rideId: subscription.rideId,
+          subscriptionDate: new Date(subscription.subscriptionDate)
+        }
+      });
+    } else {
+      throw new Error("Participants limit exceed")
+    }
+  }
+
+  async getSubscriptionsPerPedal(pedalId: string): Promise<SubscriptionPerPedal> {
+    const pedal = await this.prismaPedalsRepository.findUnique(pedalId);
+
+    if (!pedal)
+      throw new Error("Pedal " + pedalId + " not found")
+
+    const subscritiponsPerPedal = await this.prisma.subscription.findMany({
+      where: {
+        rideId: pedalId
+      }
+    })
+  
+    const users = subscritiponsPerPedal
+      .filter( subscriptionPerPedal => {
+        return subscriptionPerPedal.rideId === pedalId
+      })
+      .map(subscription => {
+        return { id: subscription.userId }
+      })
+
+    return Object.assign({}, {pedalId}, { users } )
   }
 }
