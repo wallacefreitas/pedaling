0bdab5096b21f82ad311d3a4a4b0d2adfee4fc94
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-19 20:42:44 -0300

    chore: Installed React lib

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,36 @@
+import Login from "./pages/login";
+
+export default function App() {
+  async function handleLogin() {
+    const data = {
+      username: "admin",
+      password: "admin",
+    }
+
+    const token = await fetch('http://localhost:3001/auth', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => data.token)
+
+    console.log(token);
+  }
+
+  return (
+    <div className="App">
+      <div className="flex items-center h-screen bg-[#F3E8FF]">
+        <Login />
+      </div>
+      {/* <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <button onClick={handleLogin}>
+        Login
+      </button> */}
+    </div>
+  )
+}
