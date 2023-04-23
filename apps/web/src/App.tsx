10d5f0bb39ab04184483de725b4478cfb883c923
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-23 13:48:45 -0300

    refactor: Refactor App component and moved login logic to component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,36 +1,9 @@
 import Login from "./pages/login";
 
 export default function App() {
-  async function handleLogin() {
-    const data = {
-      username: "admin",
-      password: "admin",
-    }
-
-    const token = await fetch('http://localhost:3001/auth', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(data => data.token)
-
-    console.log(token);
-  }
-
   return (
     <div className="App">
-      <div className="flex items-center h-screen bg-[#ADA2FF]">
-        <Login />
-      </div>
-      {/* <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <button onClick={handleLogin}>
-        Login
-      </button> */}
+      <Login />
     </div>
   )
 }
