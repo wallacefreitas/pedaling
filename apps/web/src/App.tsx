96ee6ea4eb8081ee91ced57166291c9fd8ae9450
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-04-28 08:46:51 -0300

    fix: Adjusts in HomePage to verify credentials and token

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,22 @@
+import Layout from "./layout";
+import Home from "./pages/home";
 import Login from "./pages/login";
+import { useUserStore } from "./store/useUserStore";
 
 export default function App() {
+  const { user } = useUserStore();
+  
+  function HomePage() {
+    return (
+      <Layout>
+        <Home />
+      </Layout>
+    )
+  }
+
   return (
     <div className="App">
-      <Login />
+      { !user.token ? <Login /> : <HomePage /> }
     </div>
   )
 }
