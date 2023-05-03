f5e9981f6fcd0d91d864cd9802fb0f0a41cd5222
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-05-03 20:14:46 -0300

    feat: Added Footer and Header components

---
+++ apps/web/src/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex h-max bg-[#9384D1] px-2">
+      Footer
+    </div>
+  )
+}
---
+++ apps/web/src/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex bg-[#9384D1] h-max p-2">
+      Header
+    </header>
+  )
+}
