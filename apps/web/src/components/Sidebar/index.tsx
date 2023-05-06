abbd9c7261c9ee9422c57e96d7e66fd5b05736ba
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-05-06 15:23:30 -0300

    feat: Add sidebar component

---
+++ apps/web/src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <div className="w-1/5 border-2 border-red-500">
+      <ul className="list-none">
+        <li>Dashboard</li>
+        <li>Pedalings</li>
+      </ul>
+    </div>
+  )
+}
