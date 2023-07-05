5bcec056f25109e8240ff1b17a80ca098330f292
Wallace de Freitas <wallace.freitas.b@gmail.com>
2023-07-05 17:41:39 -0300

    feat: add toggle component

--- apps/web/src/layout/Header.tsx
+++ apps/web/src/layout/Header.tsx
@@ -1,7 +1,10 @@
+import ToggleTheme from "../components/ToggleTheme";
+
 export default function Header() {
   return (
-    <header className="flex bg-[#9384D1] h-max p-2">
+    <header className="flex bg-[#9384D1] dark:bg-[#1C1C28] h-14 p-4 w-full justify-between">
       Header
+      <ToggleTheme />
     </header>
   )
 }
